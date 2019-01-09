f7b8f30b7ee9f7c24a28b6fbcca3642a14969a25
leeseongsu <harunokaje33@gmail.com>
2019-01-09 17:57:10 +0900

    boardController 일부 변경

--- SmartBaby/src/main/java/edu/iot/app/controller/BoardController.java
+++ SmartBaby/src/main/java/edu/iot/app/controller/BoardController.java
@@ -1,10 +1,10 @@
 package edu.iot.app.controller;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,13 +16,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.iot.common.exception.PasswordMissmatchException;
 import edu.iot.common.model.Board;
 import edu.iot.common.service.BoardService;
-import edu.iot.common.util.Util;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
@@ -31,74 +31,108 @@ import lombok.extern.slf4j.Slf4j;
 public class BoardController {
 	@Autowired
 	BoardService service;
-	
+
 	@Autowired
 	ServletContext context;
-	
+
 	@RequestMapping("/list")
-	public void list(
-			@RequestParam(value="page", defaultValue="1") int page,
-			Model model) throws Exception {
-		
+	public void list(@RequestParam(value = "page", defaultValue = "1") int page, Model model) throws Exception {
+
 //		model.addAttribute("today", Util.getToday());
 		model.addAllAttributes(service.getPage(page));
 	}
-	
-	@RequestMapping(value="/create", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/create", method = RequestMethod.GET)
 	public void createForm(Board board) {
 	}
-	
-	@RequestMapping(value="/create", method=RequestMethod.POST)
-	public String createSubmit(
-			@Valid Board board, BindingResult result,
-			MultipartHttpServletRequest request) throws Exception {
-		
-		if(result.hasErrors()) return "board/create";
-		
+
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public String createSubmit(@Valid Board board, BindingResult result, MultipartHttpServletRequest request)
+			throws Exception {
+
+		if (result.hasErrors())
+			return "board/create";
+
 		service.create(board);
-		
+
 		return "redirect:list";
 	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/android/create")
+	public @ResponseBody String androidCreate(HttpServletRequest httpServletRequest) throws Exception {
+		String userId = httpServletRequest.getParameter("userId");
+		String flag = httpServletRequest.getParameter("flag");
+
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yy/MM/dd HH:mm:ss");
+		String result = "";
+
+		if (flag.equals("1")) {
+			// userId, sleepTime 추가하여 insert
+
+			System.out.println(userId + " 아기의 잠든 시간 : ");
+			result = "아기의 잠든 시간 : " + sdf.format(date);
+		}
+		if (flag.equals("2")) {
+			// userId 확인해서 wakeupTime update하기
+
+			System.out.println(userId + " 아기의 기상 시간 : ");
+			result = "아기의 기상 시간 : " + sdf.format(date);
+		}
+		return result;
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/android/list")
+	public @ResponseBody String androidList(HttpServletRequest httpServletRequest) throws Exception {
+		Board board = new Board();
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonString = mapper.writeValueAsString(board);
+		return jsonString;
+	}
 	
-	@RequestMapping(value="/view/{boardId}", method=RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET, value = "/android/view/{boardId}")
+	public @ResponseBody String androidView(HttpServletRequest httpServletRequest) throws Exception {
+		Long boardId = Long.parseLong(httpServletRequest.getParameter("boardId"));
+		Board board = service.findById(boardId);
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonString = mapper.writeValueAsString(board);
+		return jsonString;
+	}
+
+	@RequestMapping(value = "/view/{boardId}", method = RequestMethod.GET)
 	public String view(@PathVariable long boardId, Model model) throws Exception {
 		Board board = service.findById(boardId);
 		model.addAttribute("board", board);
-		
+
 		return "board/view";
 	}
-	
-	
-	@RequestMapping(value="/edit/{boardId}", method=RequestMethod.GET)
-	public String editForm(@PathVariable int boardId, Model model) 
-													throws Exception {
+
+	@RequestMapping(value = "/edit/{boardId}", method = RequestMethod.GET)
+	public String editForm(@PathVariable int boardId, Model model) throws Exception {
 		Board board = service.findById(boardId);
 		model.addAttribute("board", board);
 		return "board/edit";
 	}
-	
-	@RequestMapping(value="/edit/{boardId}", method=RequestMethod.POST)
-	public String editSubmit(@Valid Board board, BindingResult result,
-			@RequestParam("page") int page,
+
+	@RequestMapping(value = "/edit/{boardId}", method = RequestMethod.POST)
+	public String editSubmit(@Valid Board board, BindingResult result, @RequestParam("page") int page,
 			MultipartHttpServletRequest request) throws Exception {
-		if(result.hasErrors()) return "board/edit";
-		
+		if (result.hasErrors())
+			return "board/edit";
+
 		try {
 			service.update(board);
-		} catch(PasswordMissmatchException e) {
+		} catch (PasswordMissmatchException e) {
 			result.reject("updateFail", e.getMessage());
-			
+
 			return "board/edit";
 		}
-		
-		return "redirect:/board/view/" + board.getId() +
-						"?page=" + page;
+
+		return "redirect:/board/view/" + board.getId() + "?page=" + page;
 	}
-	
+
 	@ResponseBody
-	@RequestMapping(value="/delete",
-					method=RequestMethod.GET,
-					produces="text/plain; charset=utf8")
+	@RequestMapping(value = "/delete", method = RequestMethod.GET, produces = "text/plain; charset=utf8")
 	public String delete(Board board) {
 		try {
 			service.delete(board);
